d55907bf1ad5d1808b7afc7fb77742c2ab4f25ee
234Kabi <234kabi@gmail.com>
2023-03-29 01:07:04 +0200

    .\_printf.c

--- _printf.c
+++ _printf.c
@@ -18,13 +18,13 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	{
-	va_start(list,format);
+	va_start(list , format);
 	{
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format && format[i] ! = '\0'; i++)
 	{
-		If (format[i]!='%')
+		If(format[i] ! = '%')
 		{
-			buffer[buff_ind++]=format[i];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
@@ -40,12 +40,12 @@ int _printf(const char *format, ...)
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 					flags, width, precision, size);
-			if(printed == -1);
+			if (printed == -1);
 			return (-1);
 			printed_chars += printed;
 		}
 	}
-	print_buffer(buffer,&buff_ind);
+	print_buffer(buffer, &buff_ind);
 	va_end(list);
 	return (print_chars);
 	}
